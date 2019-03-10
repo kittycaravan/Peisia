4682f09ab411d61f302595c965489e60871fa54e
klesa <kittycaravan@gmail.com>
2019-03-10 18:21:39 +0900

    4) 현 스플래시 액티비티는 끝처리 추가

--- app/src/main/java/com/peisia/peisia/ActivitySplash.kt
+++ app/src/main/java/com/peisia/peisia/ActivitySplash.kt
@@ -14,6 +14,7 @@ class ActivitySplash : AppCompatActivity() {
 
         // 2019.3.10 sungmo.ahn 스플래시에서 5초후 액티비티메인 이동
         Handler().postDelayed({
+            finish()    // 현 스플래시 액티비티는 끝.
             startActivity<ActivityMain>()   // anko 라이브러리를 이용한 액티비티 전환.
         }, 5000)
     }
